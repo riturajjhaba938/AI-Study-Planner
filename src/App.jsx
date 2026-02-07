a3a26fe08a34f36c3a9e3fb25abf79fe08fc4ddb
riturajjhaba938 <riturajjha938@gmail.com>
2026-02-07 14:18:58 +0530

    feat: complete frontend setup with dynamic subject form and dashboard integration

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Dashboard from './components/Dashboard';
+
+function App() {
+    return (
+        <div className="App">
+            {/* We can add a simple Navbar here later if time permits */}
+            <Dashboard />
+        </div>
+    );
+}
+
+export default App;
